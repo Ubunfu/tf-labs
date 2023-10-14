6010332687ef6834d9bfe35ff0e391f95e6b3402
Ryan Allen <rallen3882@gmail.com>
2023-10-14 13:01:55 -0400

    ACG TFA lab 2

---
+++ acg-tf-associate/lab2/main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    aws = {
+        version = "5.21.0"
+    }
+  }
+}
+
+provider "aws" {
+    region = "us-east-1"
+}
+
+resource "aws_instance" "myvm" {
+    ami = "ami-0fa3d3dafc154a053" # AMZ linux 2023 arm64
+    subnet_id = "subnet-9f7267d6" # us-east-1a
+    instance_type = "a1.medium"
+    tags = {
+        Name = "my-first-tf-vm"
+    }
+    provisioner "local-exec" {
+      command = "echo \"Launched VM with ID: ${self.id}!\" > provisioner.txt"
+    }
+}
