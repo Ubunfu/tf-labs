71d1143d46396f29eab391b8cf7640535545dd45
Ryan Allen <rallen3882@gmail.com>
2023-10-14 18:08:49 -0400

    ACF TFA lab3 moving root module outputs to separate tf file

--- acg-tf-associate/lab3/main.tf
+++ acg-tf-associate/lab3/main.tf
@@ -3,7 +3,3 @@ module "server" {
   server_name = "world-1"
   server_port = 25569
 }
-
-output "server_port" {
-    value = module.server.server_port
-}
---
+++ acg-tf-associate/lab3/outputs.tf
@@ -0,0 +1,4 @@
+output "server_port" {
+    description = "The minecraft server is listening on this port"
+    value = module.server.server_port
+}
